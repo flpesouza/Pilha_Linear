77a2cc9b91bb7ac0a9418c280ce32cc430451c52
Felipe Souza <67338813+flpesouza@users.noreply.github.com>
2023-10-10 15:08:48 -0300

    Add files via upload

---
+++ Pilha.cs
@@ -0,0 +1,50 @@
+﻿using System;
+class Pilha
+{
+    private char[] array;
+    private int n;
+
+    public Pilha(int tamanho)
+    {
+        array = new char[tamanho];
+        n = 0;
+    }
+
+    public void isVazia()
+    {
+        if (n == 0)
+        {
+            Console.WriteLine("Pilha vazia");
+        }
+    }
+
+    public void InserirFim(char x)
+    {
+        if (n >= array.Length)
+        {
+            throw new Exception("Erro ao inserir!");
+        }
+        array[n] = x;
+        n++;
+    }
+
+    public char RemoverFim()
+    {
+        if (n == 0)
+        {
+            throw new Exception("Erro ao remover!");
+        }
+        n--;
+        return array[n];
+    }
+
+    public void Mostrar()
+    {
+        Console.Write("[ ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine("]");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o tamanho da pilha: ");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            Pilha pilha = new Pilha(tamanho);
+
+            bool isBemFormada = true;
+
+            Console.WriteLine("Digite a sequencia de parenteses e colchetes: ");
+            string sequencia = Console.ReadLine();
+            for (int i = 0; i < sequencia.Length; i++)
+            {
+                if (sequencia[i] == '[' || sequencia[i] == '(')
+                {
+                    pilha.InserirFim(sequencia[i]);
+                }
+                else if (sequencia[i] == ']')
+                {
+                    char desempilha = pilha.RemoverFim();
+                    if (desempilha != '[')
+                    {
+                        isBemFormada = false;
+                    }
+                }
+                else if (sequencia[i] == ')')
+                {
+                    char desempilha = pilha.RemoverFim();
+                    if (desempilha != '(')
+                    {
+                        isBemFormada = false;
+                    }
+                }
+            }
+
+            if (sequencia[tamanho-1] == '(' || sequencia[tamanho - 1] == '[')
+            {
+                isBemFormada = false;
+            }
+
+            if (isBemFormada)
+            {
+                Console.WriteLine("A sequencia é bem-formada");
+            }
+            else
+            {
+                Console.WriteLine("A sequencia é malformada");
+            }
+            Console.ReadLine();
+        }
+    }
+}
